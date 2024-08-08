b449fcfbbc439ca2aa2a3009822e6adfc39f8269
hitesharma <hitesharma3899@gmail.com>
2024-08-08 17:26:48 +0530

    Implement handler interface to serve websocket connection

---
+++ internal/handler/handler.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"log"
+	"time"
+
+	ws "github.com/gorilla/websocket"
+	"github.com/hitesharma/text-stream/internal/provider"
+	"github.com/hitesharma/text-stream/pkg/infra/websocket"
+)
+
+// TextHandler implements the Handler interface
+type TextHandler struct {
+	providerManager *provider.ProviderManager
+}
+
+// NewTextHandler initializes a new TextHandler
+func NewTextHandler(manager *provider.ProviderManager) *TextHandler {
+	return &TextHandler{providerManager: manager}
+}
+
+// ServeWsConn handles WebSocket connection and streams data
+func (h *TextHandler) ServeWsConn(ctx *websocket.Context) {
+	defer ctx.Conn.Close()
+
+	for {
+		response, err := h.providerManager.GetCurrentProvider()()
+		if err != nil {
+			log.Printf("Error: %v", err)
+			h.providerManager.SwitchProvider()
+			continue
+		}
+
+		// Send message to the WebSocket client
+		err = ctx.Conn.WriteMessage(ws.TextMessage, []byte(response))
+		if err != nil {
+			log.Println("Error writing to WebSocket:", err)
+			break
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
